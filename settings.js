5b7e23c84ad211e224c3e04a9c4f604a5045ba78
FAWAS.FX <110875881+fawas-fx@users.noreply.github.com>
2022-09-17 13:02:20 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['919207343986'] //ur owner number
+global.ownername = "🦄Fawas.fx" //ur owner name
+global.ytname = "YT: Guppies world aqua" //ur yt chanel name
+global.socialm = "GitHub: fawas-fx" //ur github or insta name
+global.location = "India, kerala, kollam" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
+global.owner = ['919207343986']
+global.ownertag = '919207343986' //ur tag number
+global.botname = 'FAWAS.FX' //ur bot name
+global.linkz = "https://youtube.com/c/Guppiesworldaqua" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtube.com/c/Guppiesworldaqua" //ur website to be displayed
+global.botscript = 'https://github.com/fawas-fx/CheemsBot-MD4' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.packname = "ffx Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
+global.author = "FAWAS.FX 💎" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
